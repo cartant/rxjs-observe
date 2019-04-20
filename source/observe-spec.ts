1ea284e0af64ee3314bbb33c31ae78ca9b04a26e
Nicholas Jamieson <nicholas@cartant.com>
2019-04-20 13:36:53 +1000

    chore: Use prettier.

--- source/observe-spec.ts
+++ source/observe-spec.ts
@@ -9,103 +9,104 @@ import { Observable } from "rxjs";
 import { observe } from "./observe";
 
 describe("observe", () => {
+  describe("outside a constructor", () => {
+    const job = Symbol("job");
+    class Person {
+      [job]: string;
+      constructor(public age: number, public name: string) {}
+      greet(greeting: string): string {
+        return `${greeting}, ${this.name}.`;
+      }
+    }
 
-    describe("outside a constructor", () => {
-
-        const job = Symbol("job");
-        class Person {
-            [job]: string;
-            constructor(public age: number, public name: string) {}
-            greet(greeting: string): string { return `${greeting}, ${this.name}.`; }
-        }
-
-        it("should observe properties", () => {
-            const person = new Person(32, "Alice");
-            const { observables, proxy } = observe(person);
-            const values: (number | string)[] = [];
-            observables.age.subscribe(value => values.push(value));
-            observables.name.subscribe(value => values.push(value));
-            expect(values).to.deep.equal([32, "Alice"]);
-            proxy.age = 42;
-            proxy.name = "Bob";
-            expect(values).to.deep.equal([32, "Alice", 42, "Bob"]);
-        });
-
-        it("should observe functions", () => {
-            const person = new Person(32, "Alice");
-            const { observables, proxy } = observe(person);
-            const calls: any[][] = [];
-            observables.greet.subscribe(args => calls.push(args));
-            expect(calls).to.deep.equal([]);
-            proxy.greet("Hi");
-            expect(calls).to.deep.equal([["Hi"]]);
-        });
-
-        it("should replay properties", () => {
-            const person = new Person(32, "Alice");
-            const { observables, proxy } = observe(person);
-            proxy.age = 42;
-            proxy.name = "Bob";
-            const values: (number | string)[] = [];
-            observables.age.subscribe(value => values.push(value));
-            observables.name.subscribe(value => values.push(value));
-            expect(values).to.deep.equal([42, "Bob"]);
-        });
-
-        it("should not replay functions", () => {
-            const person = new Person(32, "Alice");
-            const { observables, proxy } = observe(person);
-            proxy.greet("Hi");
-            const calls: any[][] = [];
-            observables.greet.subscribe(args => calls.push(args));
-            expect(calls).to.deep.equal([]);
-        });
-
-        it("should support symbols", () => {
-            const person = new Person(32, "Alice");
-            person[job] = "engineer";
-            const { observables } = observe(person);
-            observables[job].subscribe(value => expect(value).to.equal("engineer"));
-        });
+    it("should observe properties", () => {
+      const person = new Person(32, "Alice");
+      const { observables, proxy } = observe(person);
+      const values: (number | string)[] = [];
+      observables.age.subscribe(value => values.push(value));
+      observables.name.subscribe(value => values.push(value));
+      expect(values).to.deep.equal([32, "Alice"]);
+      proxy.age = 42;
+      proxy.name = "Bob";
+      expect(values).to.deep.equal([32, "Alice", 42, "Bob"]);
     });
 
-    describe("inside a constructor", () => {
-
-        class Person {
-            age$: Observable<number>;
-            name$: Observable<string>;
-            greet$: Observable<[string]>;
-            constructor(public age: number, public name: string) {
-                const { observables, proxy } = observe<Person>(this);
-                this.age$ = observables.age;
-                this.name$ = observables.name;
-                this.greet$ = observables.greet;
-                return proxy;
-            }
-            greet(greeting: string): string { return `${greeting}, ${this.name}.`; }
-        }
-
-        it("should observe properties", () => {
-            const person = new Person(32, "Alice");
-            expect(person).to.have.property("age$");
-            expect(person).to.have.property("name$");
-            const values: (number | string)[] = [];
-            person.age$.subscribe(value => values.push(value));
-            person.name$.subscribe(value => values.push(value));
-            expect(values).to.deep.equal([32, "Alice"]);
-            person.age = 42;
-            person.name = "Bob";
-            expect(values).to.deep.equal([32, "Alice", 42, "Bob"]);
-        });
-
-        it("should observe functions", () => {
-            const person = new Person(32, "Alice");
-            expect(person).to.have.property("greet$");
-            const calls: any[][] = [];
-            person.greet$.subscribe(args => calls.push(args));
-            expect(calls).to.deep.equal([]);
-            person.greet("Hi");
-            expect(calls).to.deep.equal([["Hi"]]);
-        });
+    it("should observe functions", () => {
+      const person = new Person(32, "Alice");
+      const { observables, proxy } = observe(person);
+      const calls: any[][] = [];
+      observables.greet.subscribe(args => calls.push(args));
+      expect(calls).to.deep.equal([]);
+      proxy.greet("Hi");
+      expect(calls).to.deep.equal([["Hi"]]);
     });
+
+    it("should replay properties", () => {
+      const person = new Person(32, "Alice");
+      const { observables, proxy } = observe(person);
+      proxy.age = 42;
+      proxy.name = "Bob";
+      const values: (number | string)[] = [];
+      observables.age.subscribe(value => values.push(value));
+      observables.name.subscribe(value => values.push(value));
+      expect(values).to.deep.equal([42, "Bob"]);
+    });
+
+    it("should not replay functions", () => {
+      const person = new Person(32, "Alice");
+      const { observables, proxy } = observe(person);
+      proxy.greet("Hi");
+      const calls: any[][] = [];
+      observables.greet.subscribe(args => calls.push(args));
+      expect(calls).to.deep.equal([]);
+    });
+
+    it("should support symbols", () => {
+      const person = new Person(32, "Alice");
+      person[job] = "engineer";
+      const { observables } = observe(person);
+      observables[job].subscribe(value => expect(value).to.equal("engineer"));
+    });
+  });
+
+  describe("inside a constructor", () => {
+    class Person {
+      age$: Observable<number>;
+      name$: Observable<string>;
+      greet$: Observable<[string]>;
+      constructor(public age: number, public name: string) {
+        const { observables, proxy } = observe<Person>(this);
+        this.age$ = observables.age;
+        this.name$ = observables.name;
+        this.greet$ = observables.greet;
+        return proxy;
+      }
+      greet(greeting: string): string {
+        return `${greeting}, ${this.name}.`;
+      }
+    }
+
+    it("should observe properties", () => {
+      const person = new Person(32, "Alice");
+      expect(person).to.have.property("age$");
+      expect(person).to.have.property("name$");
+      const values: (number | string)[] = [];
+      person.age$.subscribe(value => values.push(value));
+      person.name$.subscribe(value => values.push(value));
+      expect(values).to.deep.equal([32, "Alice"]);
+      person.age = 42;
+      person.name = "Bob";
+      expect(values).to.deep.equal([32, "Alice", 42, "Bob"]);
+    });
+
+    it("should observe functions", () => {
+      const person = new Person(32, "Alice");
+      expect(person).to.have.property("greet$");
+      const calls: any[][] = [];
+      person.greet$.subscribe(args => calls.push(args));
+      expect(calls).to.deep.equal([]);
+      person.greet("Hi");
+      expect(calls).to.deep.equal([["Hi"]]);
+    });
+  });
 });
--- source/observe.ts
+++ source/observe.ts
@@ -5,50 +5,58 @@
 
 import { BehaviorSubject, Observable, Subject } from "rxjs";
 
-export function observe<T extends object>(instance: T): {
-    observables: {
-        [K in keyof T]: T[K] extends (...args: infer U) => any ? Observable<U> : Observable<T[K]>
-    },
-    proxy: T
+export function observe<T extends object>(
+  instance: T
+): {
+  observables: {
+    [K in keyof T]: T[K] extends (...args: infer U) => any
+      ? Observable<U>
+      : Observable<T[K]>
+  };
+  proxy: T;
 } {
-    const subjects = new Map<string | symbol, Subject<any>>();
-    const proxy = new Proxy(instance, {
+  const subjects = new Map<string | symbol, Subject<any>>();
+  const proxy = new Proxy(instance, {
+    get(target: any, name: string | symbol): any {
+      let value = target[name];
+      if (typeof value === "function") {
+        value = function(this: any, ...args: any[]): any {
+          const result = target[name].apply(this, args);
+          const subject = subjects.get(name);
+          if (subject) {
+            subject.next(args);
+          }
+          return result;
+        };
+      }
+      return value;
+    },
+    set(target: any, name: string | symbol, value: any): boolean {
+      target[name] = value;
+      const subject = subjects.get(name);
+      if (subject) {
+        subject.next(value);
+      }
+      return true;
+    }
+  });
+  return {
+    observables: new Proxy(
+      {},
+      {
         get(target: any, name: string | symbol): any {
-            let value = target[name];
-            if (typeof value === "function") {
-                value = function (this: any, ...args: any[]): any {
-                    const result = target[name].apply(this, args);
-                    const subject = subjects.get(name);
-                    if (subject) {
-                        subject.next(args);
-                    }
-                    return result;
-                };
-            }
-            return value;
-        },
-        set(target: any, name: string | symbol, value: any): boolean {
-            target[name] = value;
-            const subject = subjects.get(name);
-            if (subject) {
-                subject.next(value);
-            }
-            return true;
+          let subject = subjects.get(name);
+          if (!subject) {
+            subject =
+              typeof instance[name] === "function"
+                ? new Subject<any>()
+                : new BehaviorSubject<any>(instance[name]);
+            subjects.set(name, subject);
+          }
+          return subject.asObservable();
         }
-    });
-    return {
-        observables: new Proxy({}, {
-            get(target: any, name: string | symbol): any {
-                let subject = subjects.get(name);
-                if (!subject) {
-                    subject = (typeof instance[name] === "function") ?
-                        new Subject<any>() :
-                        new BehaviorSubject<any>(instance[name]);
-                    subjects.set(name, subject);
-                }
-                return subject.asObservable();
-            }
-        }),
-        proxy
-    };
+      }
+    ),
+    proxy
+  };
 }
